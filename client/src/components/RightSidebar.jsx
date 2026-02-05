aefbb44d9438a5eab0b7b9c52d1661426451aaf0
Mohammad kaif <mohadkaif122344@gmail.com>
2026-02-05 18:16:06 +0530

    project completed

---
+++ client/src/components/RightSidebar.jsx
@@ -0,0 +1,80 @@
+import React, { useContext, useEffect, useState } from "react";
+import assets from "../assets/assets";
+import { ChatContext } from "../context/ChatContext";
+import { AuthContext } from "../context/AuthContext";
+
+const RightSidebar = () => {
+  const { selectedUser, messages, showRightSidebar, setShowRightSidebar } =
+    useContext(ChatContext);
+  const { logout, onlineUsers } = useContext(AuthContext);
+  const [msgImages, setMsgImages] = useState([]);
+
+  // get all the images from the messages and get them to state
+  useEffect(() => {
+    setMsgImages(messages.filter((msg) => msg.image).map((msg) => msg.image));
+  }, [messages]);
+
+  if (!selectedUser) return null;
+
+  return (
+    selectedUser && (
+      <div
+        className={`
+    bg-[#8185B2]/10 text-white w-full relative overflow-y-auto max-md:bg-[#1F1B3A]/90
+   max-md:backdrop-blur-lg
+    max-md:fixed max-md:top-0 max-md:right-0 max-md:h-full max-md:w-full
+    max-md:z-50 
+    ${showRightSidebar ? "max-md:translate-x-0" : "max-md:translate-x-full"}
+  `}
+      >
+        <button
+          onClick={() => setShowRightSidebar(false)}
+          className="absolute top-4 right-4 md:hidden text-white text-xl cursor-pointer"
+        >
+          ✕
+        </button>
+
+        <div className="pt-16 flex flex-col items-center gap-2 text-xs font-light mx-auto">
+          <img
+            src={selectedUser?.profilePic || assets.avatar_icon}
+            alt=""
+            className="w-20 aspect-square rounded-full"
+          />
+          <h1 className="px-10 text-xl font-medium mx-auto flex items-center gap-2">
+            {onlineUsers.includes(selectedUser._id) && (
+              <p className="w-2 h-2 rounded-full bg-green-500"></p>
+            )}
+            {selectedUser.fullName}
+          </h1>
+          <p className="px-10 mx-auto">{selectedUser.bio}</p>
+        </div>
+
+        <hr className="border-[#ffffff50]" />
+
+        <div className="px-5 text-xs">
+          <p className="font-bold text-2xl underline">Media</p>
+          <div className="mt-2  overflow-y-scroll grid grid-cols-2 gap-4 opacity-80">
+            {msgImages.map((url, index) => (
+              <div
+                key={index}
+                onClick={() => window.open(url)}
+                className="cursor-pointer rounded"
+              >
+                <img src={url} alt="" className="h-full rounded-md" />
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <button
+          onClick={() => logout()}
+          className="absolute bottom-5 left-1/2 transform -translate-x-1/2 bg-linear-to-r from-purple-400 to-violet-600 text-white border-none text-sm font-light py-2 px-20 rounded-full cursor-pointer"
+        >
+          Logout
+        </button>
+      </div>
+    )
+  );
+};
+
+export default RightSidebar;
---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,23 @@
+import React, { useContext } from "react";
+import Sidebar from "../components/Sidebar";
+import ChatContainer from "../components/ChatContainer";
+import RightSidebar from "../components/RightSidebar";
+import { ChatContext } from "../context/ChatContext";
+
+const HomePage = () => {
+  const { selectedUser } = useContext(ChatContext);
+
+  return (
+    <div className="border w-full h-screen sm:px-[13%] sm:py-[1%]">
+      <div
+        className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-full grid grid-cols-1 relative ${selectedUser ? "md:grid-cols-[fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]" : "md:grid-cols-2"}`}
+      >
+        <Sidebar />
+        <ChatContainer />
+        <RightSidebar />
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
