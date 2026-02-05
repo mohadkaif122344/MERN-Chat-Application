5fa547bb5a3e0546df71c7bc3f4484d5bcb6675a
Mohammad kaif <mohadkaif122344@gmail.com>
2026-02-06 00:09:39 +0530

    project completed

--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -1,12 +1,14 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.js";
-import { getMessages, getUsersForSidebar, markMessageAsSeen, sendMessage } from "../controllers/messageController.js";
+import { getMessages, getUsersForSidebar, markMessageAsSeen } from "../controllers/messageController.js";
 
 
 const messageRouter = express.Router();
 
 messageRouter.get("/users", protectRoute, getUsersForSidebar);
+
 messageRouter.get("/:id", protectRoute, getMessages);
-messageRouter.post("/send/:id", protectRoute, sendMessage);
+
+messageRouter.put("/mark/:id", protectRoute, markMessageAsSeen);
 
 export default messageRouter;
