e8abab2b86202e0e2eeb068150d988cd3762c1ad
Mohammad kaif <mohadkaif122344@gmail.com>
2026-02-05 23:32:33 +0530

    project completed

--- server/server.js
+++ server/server.js
@@ -52,13 +52,9 @@ app.use("/api/auth", userRouter);
 app.use("/api/messages", messageRouter);
 
 
-if (process.env.NODE_ENV !== "production") {
   const PORT = process.env.PORT || 5000;
   
   server.listen(PORT, ()=>{
       console.log(`Server is running on http://localhost:${PORT}`);
   })
-}
 
-//Export server for Vercel
-export default server;
